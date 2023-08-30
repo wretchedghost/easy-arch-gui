ca01045887ccea3c626e4c4fa7f258a54b7d9c4a
wretchedghost <wretchedghost0@gmail.com>
2023-08-30 14:41:06 -0600

    continuing build of script

--- easy-arch-i3.sh
+++ easy-arch-i3.sh
@@ -26,4 +26,27 @@ error_print () {
     echo -e "${BOLD}${BRED}[ ${BBLUE}•${BRED} ] $1${RESET}"  
 }
 
-info_print "Let's install all the things to get i3 working. 
+info_print "Let's install all the things to get i3 working. Warning this will flatten your current i3 configs."
+
+if [ -x "$(command -v yay)" ]; then
+    print "Found yay install"
+else
+    print "Will install yay now."
+    git clone https://aur.archlinux/yay.git
+    cd yay
+    makepgk -fsri
+    cd ..
+    rm -rf yay/
+fi
+
+pacman -S --needed $(cat deps.pacman)
+yay -S --noconfirm --needed $(cat deps.yay)
+
+# Folder to build 
+# ~/.config
+# ~/.config/rofi
+# ~/.config/dunst
+# ~/.config/redshift
+# ~/.vim
+
+# Install yay (Yet Another Yogurt) AUR Helper written in Go
